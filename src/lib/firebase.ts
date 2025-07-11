bc242b6139c6ea260bbbc3b69a3c2a4edb7ec59f
Mel Ngwenya <mn.1303@gmail.com>
2025-07-11 11:20:52 +0000

    Same error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,19 +6,13 @@ const firebaseConfig = {
   apiKey: "AIzaSyBPp07PuQ7-Y4ZHecsWJNPjQaZ7wmHUsTQ",
   authDomain: "context-intelligence.firebaseapp.com",
   projectId: "context-intelligence",
-  storageBucket: "context-intelligence.firebasestorage.app",
+  storageBucket: "context-intelligence.appspot.com",
   messagingSenderId: "702465558183",
   appId: "1:702465558183:web:76df1ac90c9e7d82ea3d82",
   measurementId: "G-8Z4LX7NNV7"
 };
 
 // Initialize Firebase
-let firebaseApp;
-if (!getApps().length) {
-    firebaseApp = initializeApp(firebaseConfig);
-} else {
-    firebaseApp = getApp();
-}
-
+const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export { firebaseApp };
